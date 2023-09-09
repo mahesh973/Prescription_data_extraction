bcf5516dc4d7987a1a64239295f778fda5e65e37
Mahesh Babu <maheshchowdary973@gmail.com>
2023-09-09 15:58:02 -0700

    added class methods to presciption parser

--- backend/src/parser_prescription.py
+++ backend/src/parser_prescription.py
@@ -1,13 +1,58 @@
 from parser_generic import MedicalDocParser
+import re
 
 class PrescriptionParser(MedicalDocParser):
     def __init__(self,text):
-        MedicalDocParser.__init__(self,text)
+        super().__init__(text)
 
     def parse(self):
+        return {
+            'patient_name' : self.get_name(),
+            'address' : self.get_address(),
+            'medicine' : self.get_medicine()
+        }
+
+    def get_name(self):
+        pattern = 'Name:(.*)Date'
+        matches = re.findall(pattern, self.text)
+        if len(matches) > 0:
+            return matches[0].strip()
+    
+    def get_address(self):
+        pattern = 'Address:(.*)\n'
+        matches = re.findall(pattern, self.text)
+        if len(matches) > 0:
+            return matches[0].strip()
+
+    def get_medicine(self):
+        pattern = 'Address[^\n]*(.*)Directions'
+        matches = re.findall(pattern, self.text, flags = re.DOTALL)
+        if len(matches) > 0:
+            return matches[0].strip()
+          
+    def get_directions(self):
+        pass
+
+    def get_refill(self):
         pass
 
 
 
-if __name__ == "__main__":
-    PrescriptionParser("abc") 
+if __name__ == "__main__": 
+    document_text = '''Dr John Smith, M.D
+
+                    2 Non-Important street,
+                    New York, Phone (900)-a23- ~2222
+
+                    Name:  Virat Kohli Date: 2/05/2022
+
+                    Address: 2 cricket blvd, New Delhi
+
+                    | Omeprazole 40 mg
+
+                    Directions: Use two tablets daily for three months
+
+                    Refill: 3 times'''
+
+    obj = PrescriptionParser(document_text) 
+    print(obj.parse())
