3858d94b26bb08c28d86a8dc54bca7f3c7d899b2
Mahesh Babu <maheshchowdary973@gmail.com>
2023-09-09 16:46:56 -0700

    Refactored parser_prescriptor class code

--- backend/src/parser_prescription.py
+++ backend/src/parser_prescription.py
@@ -7,59 +7,29 @@ class PrescriptionParser(MedicalDocParser):
 
     def parse(self):
         return {
-            'patient_name' : self.get_name(),
-            'address' : self.get_address(),
-            'medicine' : self.get_medicine(),
-            'directions' : self.get_directions(),
-            'refill' : self.get_refill()
+            'patient_name' : self.get_field('patient_name'),
+            'patient_address' : self.get_field('patient_address'),
+            'medicines' : self.get_field('medicines'),
+            'directions' : self.get_field('directions'),
+            'refill' : self.get_field('refills')
+        }
+    
+    def get_field(self,field_name):
+        pattern_dict = {
+            'patient_name' : {'pattern' : 'Name:(.*)Date','flags' : 0},
+            'patient_address' : {'pattern' : 'Address:(.*)\n','flags' : 0},
+            'medicines' : {'pattern' : 'Address[^\n]*(.*)Directions','flags' : re.DOTALL},
+            'directions' : {'pattern' : 'Directions:(.*)Refill','flags' : re.DOTALL},
+            'refills' : {'pattern' : 'Refill:(.*)times','flags' : 0}   
         }
 
-    def get_name(self):
-        pattern = 'Name:(.*)Date'
-        matches = re.findall(pattern, self.text)
-        if len(matches) > 0:
-            return matches[0].strip()
-    
-    def get_address(self):
-        pattern = 'Address:(.*)\n'
-        matches = re.findall(pattern, self.text)
-        if len(matches) > 0:
-            return matches[0].strip()
-
-    def get_medicine(self):
-        pattern = 'Address[^\n]*(.*)Directions'
-        matches = re.findall(pattern, self.text, flags = re.DOTALL)
-        if len(matches) > 0:
-            return matches[0].strip()
-          
-    def get_directions(self):
-        pattern = 'Directions:(.*)Refill'
-        matches = re.findall(pattern, self.text, flags = re.DOTALL)
-        if len(matches) > 0:
-            return matches[0].strip()
-
-    def get_refill(self):
-        pattern = 'Refill:(.*)times'
-        matches = re.findall(pattern, self.text)
-        if len(matches) > 0:
-            return matches[0].strip()
+        pattern_object = pattern_dict.get(field_name)
+        if pattern_object:
+            matches = re.findall(pattern_object['pattern'], self.text, flags = pattern_object['flags'])
+            if len(matches) > 0:
+                return matches[0].strip()
 
 
 if __name__ == "__main__": 
-    document_text = '''Dr John Smith, M.D
-
-                    2 Non-Important street,
-                    New York, Phone (900)-a23- ~2222
-
-                    Name:  Virat Kohli Date: 2/05/2022
-
-                    Address: 2 cricket blvd, New Delhi
-
-                    | Omeprazole 40 mg
-
-                    Directions: Use two tablets daily for three months
-
-                    Refill: 3 times'''
-
-    obj = PrescriptionParser(document_text) 
+    obj = PrescriptionParser() 
     print(obj.parse())
